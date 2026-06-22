aa23fdf02145e2c3d833b361819d743a10fcb931
itspramodrana1 <ranapramod663@gmail.com>
2026-06-22 23:35:23 +0530

    Leetcode 643 max Subarray average 1 Siliding Window Maximum

---
+++ src/arrays/L643_maximumAverageSubarray.java
@@ -0,0 +1,55 @@
+package arrays;
+
+public class L643_maximumAverageSubarray {
+    /*
+
+    643. Maximum Average Subarray I
+    You are given an integer array nums consisting of n elements, and an integer k.
+
+    Find a contiguous subarray whose length is equal to k that has the maximum average
+    value and return this value. Any answer with a calculation error less than 10-5
+    will be accepted.
+
+    Example 1:
+    Input: nums = [1,12,-5,-6,50,3], k = 4
+    Output: 12.75000
+    Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+    Example 2:
+    Input: nums = [5], k = 1
+    Output: 5.00000
+    Constraints:
+
+    n == nums.length
+    1 <= k <= n <= 105
+   -104 <= nums[i] <= 104
+
+     */
+
+    public static double maxSubarrayAverage(int[] arr, int k){
+
+        double maxAverage = 0;
+        int windowSum = 0;
+        for(int i=0; i < k; i++){
+           windowSum += arr[i];
+        }
+        maxAverage = (double) windowSum / k;
+
+        for(int i=1; i < arr.length - k; i++){
+            windowSum = windowSum - arr[i-1] + arr[i + k - 1];
+            maxAverage = Math.max(maxAverage, (double) windowSum / k);
+        }
+        return maxAverage;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,12,-5,-6,50,3};
+
+        int k = 4;
+
+        double ans = maxSubarrayAverage(arr,k);
+
+        System.out.println(ans);
+
+    }
+}
