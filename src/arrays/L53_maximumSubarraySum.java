c9ccad313832034db4b988bba6fc8deb63a8d661
itspramodrana1 <ranapramod663@gmail.com>
2026-06-24 23:50:29 +0530

    Leetcode 53 MaxSumSubbarray Khadne Algorithm

---
+++ src/arrays/L53_maximumSubarraySum.java
@@ -0,0 +1,54 @@
+package arrays;
+
+public class L53_maximumSubarraySum {
+    /*
+
+    53. Maximum Subarray
+
+    Given an integer array nums, find the subarray with the largest sum, and return
+    its sum.
+
+    Example 1:
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+    Example 2:
+    Input: nums = [1]
+    Output: 1
+    Explanation: The subarray [1] has the largest sum 1.
+    Example 3:
+    Input: nums = [5,4,-1,7,8]
+    Output: 23
+    Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+    Constraints:
+
+     1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+    Follow up: If you have figured out the O(n) solution, try coding another
+    solution using the divide and conquer approach, which is more subtle.
+
+     */
+
+    public static int maxSum(int[] arr){
+        int currSum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            currSum += arr[i];
+            if(currSum > max){
+                max = currSum;
+            }
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5,4,-1,7,8};
+          int ans = maxSum(arr);
+        System.out.println(ans);
+
+    }
+}
