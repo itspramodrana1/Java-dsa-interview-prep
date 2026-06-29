ba210b79b27ed445de46d2b236d1ceadd42814e8
itspramodrana1 <ranapramod663@gmail.com>
2026-06-29 20:33:59 +0530

    Merge Two LinkLists

---
+++ src/linkedlist/MergeTwoLinkLists.java
@@ -0,0 +1,34 @@
+package linkedlist;
+
+public class MergeTwoLinkLists {
+
+
+
+//        public static Node mergesort(Node head){
+//            //Write your code here
+//
+//            Node temp1 = head;
+//            Node temp2 = head.next;
+//            Node t = new Node(100);
+//            Node temp = head;
+//            while(temp!=null){
+//                if(temp1.data<temp2.data){
+//                    t.next = temp1;
+//                    t = temp1;
+//                    temp1 = temp1.next;
+//                    temp = temp.next;
+//                }else{
+//                    t.next = temp2;
+//                    t = temp2;
+//                    temp2 = temp2.next;
+//                    temp = temp.next;
+//                }
+//            }
+//            return head.next;
+//        }
+
+
+    public static void main(String[] args) {
+
+    }
+}
